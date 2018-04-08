124b566e20826f3a857f76448eb099289e2c36c4
Romain Petit <contact@romainpetit.com>
2018-04-08 20:13:31 +0200

    Ajout d'un bouton dans le composant Machine, qui lance une fonction

--- app/components/Machine.js
+++ app/components/Machine.js
@@ -6,13 +6,23 @@
 import React from 'react';
 
 class Machine extends React.Component {
+  // Méthode lancée au clic sur le bouton "Toggle"
+  handleClick() {
+    console.log('Clic sur le bouton ------- ');
+    // On accède dans cette méthode à ce qu'on a passé en props au composant
+    console.log(this.props.name);
+  }
+
   render() {
     return (
         <div
-        {/*Si isActive passée en props est à true, ajouter la classe "active" */}
         className= { this.props.isActive ? "machine active" : "machine" }>
-          {/* Afficher le nom*/}
+          {/*Si isActive passée en props est à true, ajouter la classe "active" */}
           {this.props.name}
+          {/* On appelle handleClick avec (e) pour pouvoir accéder à this*/}
+          <button onClick={(e) => this.handleClick(e)} type="button" className="btn">
+            Toggle
+          </button>
         </div>
       )
   }
